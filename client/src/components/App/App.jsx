ba0e28700735c8bedb523d0b98dfac3b02867328
Adam Iverson <aciverso@gmail.com>
2022-05-31 12:51:41 -0500

    axios installed

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,8 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [test, setTest] = useState([]);
+
+  useEffect
+
+  // function Test() {
+  //   setTest
+  // }
   return (
     <div className="App">
+      <p>{!test ? "Loading..." : test}</p>
     </div>
   );
 }

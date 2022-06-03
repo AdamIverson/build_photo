abec4e1ebfae84693a925dd7caf8802d08cf50b8
Adam Iverson <aciverso@gmail.com>
2022-06-02 20:18:17 -0500

    about/contact in

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Redirect, Route, Routes } from "react-router-dom";
 import Test from "../Test/Test";
 import Nav from "../Nav/Nav";
+import About from "../About/About";
+import Contact from "../Contact/Contact";
 
 import "./App.css";
 
@@ -11,7 +13,9 @@ function App() {
       <BrowserRouter>
       <Nav />
         <Routes>
-          <Route path="/" element={ <Test /> } />
+        <Route exact path="/" element={<Test />}></Route>
+        <Route exact path="/about" element={<About />}></Route>
+        <Route exact path="/contact" element={<Contact />}></Route>
         </Routes>
       </BrowserRouter>
     </div>

f7881f659129aeaaab61b60b9f13a66735c48ee2
Adam Iverson <aciverso@gmail.com>
2022-06-05 12:52:06 -0500

    photo layout cover to contain on hover

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -4,12 +4,10 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <div className="sidenav">
-      <ul>NAVBAR</ul>
-      <Link to="/">
-      <li>home</li>
-      </Link>
-      <li><a href="/about">about</a></li>
-      <li><a href="/contact">contact</a></li>
+      <ul className="nav-list"></ul>
+      <li className="nav-list-item"><a href="/">home</a></li>
+      <li className="nav-list-item"><a href="/about">about</a></li>
+      <li className="nav-list-item"><a href="/contact">contact</a></li>
     </div>
   );
 }

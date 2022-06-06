92755c1f8bd8098af3396e3c76318dab48108046
Adam Iverson <aciverso@gmail.com>
2022-06-06 08:39:42 -0500

    big merge fun

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -4,6 +4,7 @@ import Test from "../Test/Test";
 import Nav from "../Nav/Nav";
 import About from "../About/About";
 import Contact from "../Contact/Contact";
+import MyDropzone from "../Uploader/Uploader";
 
 import "./App.css";
 
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" element={<Test />}></Route>
           <Route exact path="/about" element={<About />}></Route>
           <Route exact path="/contact" element={<Contact />}></Route>
+          <Route exact path="/uploader" element={<MyDropzone />}></Route>
         </Routes>
       </BrowserRouter>
     </div>

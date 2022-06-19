6d1ae9c0556a7d293f0f208051e371002b622cd5
Adam Iverson <aciverso@gmail.com>
2022-06-19 09:38:29 -0500

    cloudinary uploader server commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
 const express = require("express");
+const { cloudinary } = require("./utils/cloudinary");
 require('dotenv').config();
 
 const app = express();
 
-const testRouter = require('./routes/test.router'); 
+// const testRouter = require('./routes/test.router'); 
+// app.use('/api/test', testRouter);
 
-app.use('/api/test', testRouter);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// });
-
-// // Body parser middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/api/upload", async (req, res) => {
+  console.log('process.env.CLOUDINARY_API_KEY: ', process.env.CLOUDINARY_API_KEY);
+  try {
+    const fileStr = req.body.data;
+    // console.log('fileStr:', fileStr);
+    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: 'testFolder1000'
+    }); 
+    console.log("uploadedResponse:", uploadedResponse);
+    res.json({ msg: "YAY" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({err: 'bad news'})
+  }
+});
 
 // Serve static files
 app.use(express.static('build'));

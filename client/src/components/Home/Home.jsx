1179e6505e24876fd42405f6e11e89ad8eb7f15b
Adam Iverson <aciverso@gmail.com>
2022-06-30 14:16:44 -0500

    home component populated with template

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Image, Transformation } from "cloudinary-react";
+function Home() {
+  const [test, setTest] = useState([]);
 
-export default function Home() {
+  useEffect(() => {
+    callCloudinary();
+  }, []);
+
+  const callCloudinary = async () => {
+    try {
+      const res = await fetch("/api/test");
+      const data = await res.json();
+      setTest(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <h1>adam iverson photography</h1>
-  )
+    <div>
+      <h1>adam iverson photography</h1>
+      <div className="container">
+        <ul className="imageList">
+          {test?.map((imageId, index) => {
+            return (
+              <Image
+                key={index}
+                cloudName="aiphoto"
+                publicId={imageId}
+                className="image"
+              >
+                <Transformation crop="scale" width="500"/>
+                <Transformation radius="20"/>
+                <Transformation object-fit="contain"/>
+              </Image>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+export default Home;

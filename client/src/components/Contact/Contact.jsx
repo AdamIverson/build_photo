7238b32d164a922d926c7ea2ceb6895cbb06dc20
Adam Iverson <aciverso@gmail.com>
2022-07-15 20:04:51 -0500

    calendly button

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { InlineWidget } from "react-calendly";
+import { InlineWidget, PopupButton } from "react-calendly";
 import axios from "axios";
 
 export default function Contact() {
@@ -25,29 +25,42 @@ export default function Contact() {
   return (
     <div className="contactPage">
       <h1>Contact</h1>
-      <div className="contact-container">
+      <div className="container contact-container">
         <form onSubmit={(e) => sendMessage(e)}>
-              <label htmlFor="email">enter email address: </label>
-              <input
-                type="email"
-                id="email"
-                name="from"
-                value={from}
-                onChange={(e) => setFrom(e.target.value)}
-              ></input>
-              <label htmlFor="message">enter message:</label>
-              <textarea
-                id="message"
-                name="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              ></textarea>
-              <button type="submit">SUBMIT BUTTON</button>
+          <label htmlFor="email">enter email address: </label>
+          <input
+            type="email"
+            id="email"
+            name="from"
+            value={from}
+            onChange={(e) => setFrom(e.target.value)}
+          ></input>
+          <label htmlFor="message">enter message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button type="submit">SUBMIT BUTTON</button>
         </form>
       </div>
-      <div className="calendly">
-        <InlineWidget url="https://calendly.com/adamciverson"/>
+      <h2>OR</h2>
+      <div className="schedule-btn">
+        <PopupButton
+          url="https://calendly.com/adamciverson"
+          /*
+           * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+           * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+           */
+          rootElement={document.getElementById("root")}
+          text="Click here to schedule!"
+          className="schdBtn"
+        />
       </div>
+      {/* <div className="calendly">
+        <InlineWidget url="https://calendly.com/adamciverson"/>
+      </div> */}
     </div>
   );
 }

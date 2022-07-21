816bf341669cfe0b51bc4ae19e2a7fdefa3fe870
Adam Iverson <aciverso@gmail.com>
2022-07-21 09:21:04 -0500

    trying to trigger heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const pool = require("./modules/pool");
 const path = require("path");
 const app = express();
 
-//This will create a middleware.
+//This will create a middleware
 //When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 

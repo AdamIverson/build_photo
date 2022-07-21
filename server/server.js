f7087c05bf94b52df1b1fc2c1c51f02990c8bc36
Adam Iverson <aciverso@gmail.com>
2022-07-21 08:27:48 -0500

    path made to publish front end with back end

--- server/server.js
+++ server/server.js
@@ -5,8 +5,12 @@ require("dotenv").config({
   path: "./server/.env",
 });
 const pool = require("./modules/pool");
-
+const path = require("path");
 const app = express();
+
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // Body parser middleware
 app.use(bodyParser.json({limit: '50mb'}));

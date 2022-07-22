f83eb1588ec1e5bdf12c35865e762f1bfecb3969
Adam Iverson <aciverso@gmail.com>
2022-07-22 06:58:09 -0500

    link in cloudinary call back to /api/testFolder1000

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Image } from "cloudinary-react";
-require("dotenv").config({
-  path: "../../../server/.env",
-});
+// require("dotenv").config({
+//   path: "../../../server/.env",
+// });
 
 function Home() {
   const [test, setTest] = useState([]);
 
-  const cloudAPI = process.env.CLOUDINARY_URL
+  // const cloudAPI = process.env.CLOUDINARY_URL
 
   useEffect(() => {
     callCloudinary();
@@ -15,7 +15,7 @@ function Home() {
 
   const callCloudinary = async () => {
     try {
-      const res = await fetch(`${cloudAPI}/api/testFolder1000`);
+      const res = await fetch(`/api/testFolder1000`);
       const data = await res.json();
       setTest(data);
     } catch (error) {
